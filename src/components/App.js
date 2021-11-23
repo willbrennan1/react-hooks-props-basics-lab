6a9e99806f2524803ab5f5cab18730f9b453be8d
Will Brennan <willbrennan314@gmail.com>
2021-11-23 12:14:31 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,18 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
+
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username = {user.name} city = {user.city} color = {user.color}/>
+      <About 
+      bio = {user.bio}
+      github={user.links.github}
+      linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
